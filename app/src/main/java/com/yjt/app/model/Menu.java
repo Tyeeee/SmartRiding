f2f77b3e4ff630a60eb31a4a7c1a9e09e78c69bf
Tye <yuanjintye@163.com>
2016-09-01 18:12:53 +0800

    1.首页路线搜索业务逻辑初步实现

---
+++ app/src/main/java/com/yjt/app/model/Menu.java
@@ -0,0 +1,40 @@
+package com.yjt.app.model;
+
+import com.yjt.app.ui.sticky.OnGroupListener;
+
+public class Menu implements Comparable<Menu>, OnGroupListener {
+
+    private int mIcon;
+    private String mTitle;
+
+    public int getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(int mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    @Override
+    public int compareTo(Menu menu) {
+        return 0;
+    }
+
+    @Override
+    public String getGroupName() {
+        return null;
+    }
+
+    @Override
+    public long getGroupId() {
+        return 0;
+    }
+}
