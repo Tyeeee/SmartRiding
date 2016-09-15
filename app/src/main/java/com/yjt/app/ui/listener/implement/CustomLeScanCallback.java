ddf1490e4a576124fe4dcb87cd9f563716bfdb55
袁錦泰 <yuanjintye@gmail.com>
2016-09-15 23:16:58 +0800

    1.蓝牙设备扫描显示 2.Dialog问题处理

--- app/src/main/java/com/yjt/app/ui/listener/implement/CustomLeScanCallback.java
+++ app/src/main/java/com/yjt/app/ui/listener/implement/CustomLeScanCallback.java
@@ -2,26 +2,45 @@ package com.yjt.app.ui.listener.implement;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
+import android.text.TextUtils;
 
+import com.yjt.app.constant.Constant;
 import com.yjt.app.utils.LogUtil;
+import com.yjt.app.utils.MessageUtil;
 
 import java.util.ArrayList;
 
 public class CustomLeScanCallback implements BluetoothAdapter.LeScanCallback {
 
     private ArrayList<BluetoothDevice> mDevices = new ArrayList<>();
+    private Handler mHandler;
+    private boolean isScan;
+
+    public CustomLeScanCallback(Handler handler) {
+        this.mHandler = handler;
+    }
 
     @Override
     public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-        LogUtil.print("---->onLeScan:");
-        LogUtil.print("---->device:" + device.getAddress());
-        LogUtil.print("---->device:" + device.getName());
-        LogUtil.print("---->device:" + device.getType());
-        LogUtil.print("---->device:" + device.getBondState());
-        if (!mDevices.contains(device)) {
+        LogUtil.print("---->rssi:" + rssi);
+        LogUtil.print("---->address:" + device.getAddress());
+        LogUtil.print("---->name:" + device.getName());
+        LogUtil.print("---->type:" + device.getType());
+        LogUtil.print("---->state:" + device.getBondState());
+        if (!mDevices.contains(device) && !TextUtils.isEmpty(device.getName())) {
             mDevices.add(device);
-            //TODO Adapter
         }
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isScan) {
+                    isScan = !isScan;
+                    mHandler.sendMessage(MessageUtil.getMessage(Constant.Bluetooth.GET_DEVICE_LIST_SUCCESS, mDevices));
+                }
+            }
+        }, Constant.Bluetooth.SCAN_PERIOD);
     }
 }
 
